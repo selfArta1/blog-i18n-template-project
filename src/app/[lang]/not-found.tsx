ef67c990b208cc178504df679ebf06340d5b969d
thlo7777 <thlo7777@qq.com>
2024-02-03 19:59:07 +0800

    update

--- src/app/[lang]/not-found.tsx
+++ src/app/[lang]/not-found.tsx
@@ -6,7 +6,7 @@ import Negotiator from "negotiator";
 import { headers } from "next/headers";
 import Link from "next/link";
 
-function getLocale(): Locale | undefined {
+function getLocale(): string {
   // Negotiator expects plain object so we need to transform headers
   const headersList = headers();
   const referer = headersList.get("referer");
@@ -15,28 +15,26 @@ function getLocale(): Locale | undefined {
     const url = new URL(referer);
     const host = url.host;
     const path = url.pathname;
-
+    const locale = path.split("/")[1];
     // console.log("host: " + host);
     // console.log("path: " + path);
     // console.log("local: " + path.split("/")[1]);
-    return path.split("/")[1];
+    return locale;
   } else {
-    return undefined;
+    return "en";
   }
 }
 
 //That's a issue from github  see https://github.com/vercel/next.js/issues/50699
 export default async function NotFound() {
-  const mylocal: Locale = getLocale();
+  const mylocal = getLocale() as Locale;
   console.log("mylocal: " + mylocal);
-  if (mylocal) {
-    const dict = await getDictionary(mylocal);
-  }
+  const dict = await getDictionary(mylocal);
 
   return (
     <div>
       <h2>404</h2>
-      <p>dict.NotFound.content</p>
+      <p>{dict.NotFound.content}</p>
       <Link href="/">Return Home</Link>
     </div>
   );
